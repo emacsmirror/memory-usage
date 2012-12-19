cb73fe66a59d737a72a5ed4259794b1d75aefa2c
Stefan Monnier <monnier@iro.umontreal.ca>
2012-12-19 09:14:47 -0500

    * memory-usage: Bump version, belatedly.

--- memory-usage.el
+++ memory-usage.el
@@ -4,7 +4,7 @@
 
 ;; Author: Stefan Monnier <monnier@cs.yale.edu>
 ;; Keywords: maint
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
